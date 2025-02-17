0179b2d9a64728cdf51be73b27acfcc3e24ba6a9
darshan2436 <thapadarshan447@gmail.com>
2025-02-18 00:39:04 +0545

    updated the emptydata page which must direct to /add instead of routine/add

--- src/Emptydata.js
+++ src/Emptydata.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
 function Emptydata({type}) {
+  const link = `/${type}/add`
+  console.log(link)
   return (
     <div className="flex flex-col items-center">
         <h1 className="text-custom-responsive text-red-500">No {type} to display</h1>
         <Link 
-            to="/routine/add" 
+            to={link}
             className="text-3xl mt-5 text-green-400">
             Add new {type}
         </Link>
